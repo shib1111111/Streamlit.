64e184f73ecc23ae623ba7ed42e1110dda50eb1d
Shibkumar <96659370+shib1111111@users.noreply.github.com>
2022-11-30 20:20:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,9 @@ else:
 st.write(f"Answer = {ans}")
 
 
-if st.button("Submitted by Shib Kumar Saraf"):
-        st.write("Shib Kumar Saraf Email id : 21f1001520@student.onlinedegree.iitm.ac.in")
+if st.button("Calculate"):
+        st.write(f"Answer = {ans}")
+        st.write("submitted by")
+        st.write("Shib Kumar Saraf ; Email id : 21f1001520@student.onlinedegree.iitm.ac.in")
 
     
