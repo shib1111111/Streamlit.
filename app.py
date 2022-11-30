6bb068f313fec68269b353d48f4b99788d78ca52
Shibkumar <96659370+shib1111111@users.noreply.github.com>
2022-11-30 20:06:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.header("Enter the Two Numbers")
 number1 = st.number_input(label= "Enter the 1st large Number")
 number2 = st.number_input(label= "Enter the 2st small Number")
 if number2 != 0:
-        ans = num1 / num2
+        ans = number1 / number2
 else:
   st.warning("Division by 0 error. Please enter a non-zero number.")
   ans = "Not defined"
