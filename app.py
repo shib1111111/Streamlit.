f4a55188db5493387d21407a41874ea929922666
Shibkumar <96659370+shib1111111@users.noreply.github.com>
2022-11-30 19:57:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ st.header("Enter the Two Numbers")
 number1 = st.number_input("Enter the 1st large Number",step=1,format="%.2f")
 number2 = st.number_input("Enter the 2st small Number",step=1,format="%.2f")
 st.write(division)
-def division(): 
+def div(): 
   if number2!=0:
         ans = num1 / num2
-    else:
-        st.warning("Division by 0 error. Please enter a non-zero number.")
-        ans = "Not defined"
-   st.success(f"Division of Two Number are :  {ans}")
+  else:
+    st.warning("Division by 0 error. Please enter a non-zero number.")
+    ans = "Not defined"
+  st.success(f"Division of Two Number are :  {ans}")
 if st.button("Calculate result"):
-    division()
+    div()
