bf8743b7c6e96efade2d7bd55ccbc85a8486cf96
Shibkumar <96659370+shib1111111@users.noreply.github.com>
2022-11-30 19:51:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,15 @@ import streamlit as st
 import pandas as pd
 st.write('Division of Two Numbers')
 st.header("Enter the Two Numbers")
-number1 = st.number_input("Enter the 1st Number",min_value=1)
-number2 = st.number_input("Enter the 2st Number",min_value=0)
-division = number2/number1
-st.write('Division of Two Number are : ')
+number1 = st.number_input("Enter the 1st large Number",step=1,format="%.2f")
+number2 = st.number_input("Enter the 2st small Number",step=1,format="%.2f")
 st.write(division)
+def division(): 
+  if number2!=0:
+        ans = num1 / num2
+    else:
+        st.warning("Division by 0 error. Please enter a non-zero number.")
+        ans = "Not defined"
+   st.success(f"Division of Two Number are :  {ans}")
+if st.button("Calculate result"):
+    division()
